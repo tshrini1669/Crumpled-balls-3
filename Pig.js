c991e7fa02ce5056236d840e27563fe4b3fb1244
tshrini1669 <77674150+tshrini1669@users.noreply.github.com>
2021-04-21 19:49:20 +0530

    Add files via upload

--- Pig.js
+++ Pig.js
@@ -17,19 +17,19 @@ class Pig extends BaseClass {
        score=score+100 ;
     this.remove=false;
   }
-   //  push();
-     //this.Visiblity = this.Visiblity - 5;
-     //tint(255,this.Visiblity);
-     //image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-     //pop();
+    push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
    }
   }
 
- // score(){
-   // if (this.Visiblity < 0 && this.Visiblity > -1005){
-    //  score++;
-    //}
-  //}
+  score(){
+    if (this.Visiblity < 0 && this.Visiblity > -1005){
+      score++;
+    }
+  }
 
 
 
